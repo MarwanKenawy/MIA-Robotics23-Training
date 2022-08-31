033b4600b6d5dafe6fd1025f1bc2a11b708e58bc
MarwanKenawy <110904424+MarwanKenawy@users.noreply.github.com>
2022-08-31 02:26:32 +0200

    offline/task4

--- offline/task4/part2/IMU/IMU.ino
+++ offline/task4/part2/IMU/IMU.ino
@@ -4,8 +4,11 @@
 #define Gyro_ZOUT_H 0x47
 #define Gyro_CONFIG 0x1B
 int16_t gyro_z=0;
+int16_t gyro_offset_z=0;
+int nsamples=3000;
 long previoustime=0;
 long currenttime=0;
+long elapsed=0;
 void setup() {
   Serial.begin(9600);
   Wire.begin();
@@ -14,8 +17,7 @@ void setup() {
   Wire.write(0x00);
   Wire.endTransmission();
   Gyro_config();
-  
-
+  calibration();
 }
 
 void loop() {
@@ -25,12 +27,13 @@ void loop() {
   Wire.requestFrom(IMU_ADDRESS,2);
   while(Wire.available()<2);
   gyro_z = Wire.read()<<8 | Wire.read();
-  int z= gyro_z*2000/32.786;
+  int z=gyro_z-gyro_offset_z;
+  z= gyro_z*2000/32.786;
   currenttime=millis();
-  if((currenttime-previoustime)>1000){
+  if(elapsed=(currenttime-previoustime)>1000){
     previoustime=currenttime;
     Serial.print("Z : ");
-    Serial.println(z);
+    Serial.println(z*elapsed);
   }
 }
 void Gyro_config(){
@@ -39,3 +42,15 @@ void Gyro_config(){
   Wire.write(0x18);
   Wire.endTransmission();
 }
+void calibration(){
+  for(int i=0;i<nsamples;i++){
+    Wire.beginTransmission(IMU_ADDRESS);
+    Wire.write(Gyro_ZOUT_H);
+    Wire.endTransmission();
+    Wire.requestFrom(IMU_ADDRESS,2);
+    while(Wire.available()<2);
+    gyro_z = Wire.read()<<8 | Wire.read();
+    gyro_offset_z+=gyro_z;
+    }
+  gyro_offset_z/=nsamples;
+  }
